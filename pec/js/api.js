1eb2afd755d23b74e928afb3938627e37c60912a
felifade <felifade@gmail.com>
2026-03-27 00:18:55 -0600

    27 marzo

--- pec/js/api.js
+++ pec/js/api.js
@@ -2,7 +2,7 @@
 
 const USE_MOCK = false;
 // La URL que te dará Google Apps Script cuando lo publiques
-const GOOGLE_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbwB7VUrYoyzRCgGHKEsIJGSdGDgwOAx6uFtevH4HTrfCZEb0FfqUkFxHHEau_VRVUH5WA/exec";
+const GOOGLE_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbxidIzJ0KG5ArhTlcRWK10QpoeIRjx3_GIvPNPEs8rKVIz5C092oCuiCV781CUmkSoUTQ/exec";
 
 const api = {
     // Almacén temporal de datos para no recargar en cada clic
